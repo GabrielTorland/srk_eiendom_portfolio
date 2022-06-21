9d8c4921ee3a3582227737fa7f01b73e208ece81
Ole Pedersen <olepedersen.server@proton.me>
2022-06-21 15:02:38 +0200

    Contact back-end created
    Added create, delete, details, edit, and index functionalities

--- srk_website/Controllers/HomeController.cs
+++ srk_website/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using srk_website.Data;
 using srk_website.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace srk_website.Controllers
@@ -7,17 +9,20 @@ namespace srk_website.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
+            _context = context;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            ViewData["Contacts"] = await _context.Contact.ToListAsync();
             return View();
         }
-
+        
         public IActionResult Privacy()
         {
             return View();
--- srk_website/Models/Contact.cs
+++ srk_website/Models/Contact.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace srk_website.Models
 {
     public class Contact
     {
-        public Contact() {}
+        public Contact(){}
+        public Contact(string address, string zip, string city, string country, string phone, string email)
+        {
+            Address = address;
+            Zip = zip;
+            City = city;
+            Country = country;
+            Phone = phone;
+            Email = email;
+        }
 
         public int Id { get; set; }
-
+        
         [Required]
         public string Address { get; set; }
 
         [Required]
+        [StringLength(4)]
         public string Zip { get; set; }
 
         [Required]
@@ -22,9 +33,12 @@ namespace srk_website.Models
         public string Country { get; set; }
 
         [Required]
+        [StringLength(8)]
+        [Phone]
         public string Phone { get; set; }
 
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
     }
