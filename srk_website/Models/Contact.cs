21f338eab4cd4b9aa3472e160083f18678a237ff
Ole Pedersen <olepedersen.server@proton.me>
2022-06-21 12:51:16 +0200

    Contact model created

---
+++ srk_website/Models/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace srk_website.Models
+{
+    public class Contact
+    {
+        public Contact() {}
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Zip { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+    }
+}
