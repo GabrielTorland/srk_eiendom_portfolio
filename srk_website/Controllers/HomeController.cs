82e2561918fcb77e0453adeafd5595451b151d4f
Ole Pedersen <olepedersen.server@proton.me>
2022-06-22 19:02:14 +0200

    Image slide show
    The admin can now add a new image or delete an exisiting image in slide show

--- srk_website/Controllers/HomeController.cs
+++ srk_website/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using srk_website.Data;
 using srk_website.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using srk_website.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace srk_website.Controllers
 {
@@ -10,15 +12,39 @@ namespace srk_website.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
+        private readonly IAzureStorage _storage;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, IAzureStorage storage)
         {
+            _storage = storage;
             _context = context;
             _logger = logger;
         }
-
+        
         public async Task<IActionResult> Index()
         {
+            // Get the uri for all images in the azure container.
+            var files = await _storage.ListAsync();
+            // List of image meta-data.
+            List<IDictionary<string, string>> images = new List<IDictionary<string, string>>();
+            foreach (var file in files)
+            {
+                // See if the image is in the database.
+                var img = await _context.ImageSlideShow.FindAsync(file.Name);
+                // If the image is not a ImageSlideShow then we check the next image.
+                if (img == null)
+                {
+                    continue;
+                }
+                // Image meta-data.
+                IDictionary<string, string> tmpDict = new Dictionary<string, string>();
+                tmpDict.Add("ProjectName", img.ProjectName);
+                tmpDict.Add("Country", img.City);
+                tmpDict.Add("Uri", file.Uri);
+                tmpDict.Add("Website", img.Website.ToString());
+                images.Add(tmpDict);
+            }
+            ViewData["Images"] = images;
             ViewData["Contacts"] = await _context.Contact.ToListAsync();
             return View();
         }
