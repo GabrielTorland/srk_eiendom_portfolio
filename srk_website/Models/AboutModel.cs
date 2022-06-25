d787247b4ea021fc6898fc0f6830e599e1784668
Ole Pedersen <olepedersen.server@proton.me>
2022-06-25 20:30:58 +0200

    About page
    Added jquery library for txt editor. About page can now be viewed and edited

---
+++ srk_website/Models/AboutModel.cs
@@ -0,0 +1,15 @@
+﻿namespace srk_website.Models
+{
+    public class AboutModel
+    {
+        public AboutModel() { }
+        public AboutModel(string text)
+        {
+            Text = text;
+        }
+        
+        public int Id { get; set; }
+        
+        public string Text { get; set; }
+    }
+}
