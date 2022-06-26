9d2ce77aa9e470085c9d9e05be8d5192b8b5bae2
Ole Pedersen <olepedersen.server@proton.me>
2022-06-26 17:57:35 +0200

    Images can now be uploaded(returns URI) and deleted. There is also an index page which shows all the URIs associated with each image.

---
+++ srk_website/Models/StorageModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace srk_website.Models
+{
+    public class StorageModel
+    {
+        public StorageModel() { }
+        
+        public StorageModel(string imageName, string imageUri)
+        {
+            ImageName = imageName;
+            ImageUri = imageUri;
+        }
+        
+        public int Id { get; set; }
+        
+        public string ImageName { get; set; }
+
+        [Url]
+        public string ImageUri { get; set; }
+
+    }
+}
