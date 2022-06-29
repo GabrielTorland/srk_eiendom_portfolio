b0e821647ed701db984ac3fc624fd4ae9e3c0b7c
Ole Pedersen <olepedersen.server@proton.me>
2022-06-29 15:46:46 +0200

    team page

---
+++ Models/TeamModel.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace srk_website.Models
+{
+    public class TeamModel
+    {
+        public TeamModel() { }
+        
+        public TeamModel(string firstName, string lastName, string position, string email, string phone, string linkedIn, IFormFile file)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Position = position;
+            Email = email;
+            Phone = phone;
+            LinkedIn = linkedIn;
+        }
+
+        public int Id { get; set; }
+
+        [DisplayName("First name")]
+        [RegularExpression("[A-Z][a-zA-Z]+")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last name")]
+        [RegularExpression("[A-Z][a-zA-Z]+")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(50)]
+        public string Position { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        [Url]
+        public string LinkedIn { get; set; }
+
+        [ValidateNever]
+        public string ImageName { get; set; }
+
+        [ValidateNever]
+        public string Uri { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + ' ' + LastName;
+            }
+        }
+    }
+}
