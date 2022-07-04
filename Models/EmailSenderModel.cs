3eeccc7ea7e4b751e8e093e484caf8feb0623262
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2022-07-04 18:54:30 +0200

    Email section is now implemented
    I send the form using jquery. Controller is CSRF protected. Had to add anti fogery key manually in the header. If there are any errors they are displayed when submitting. Did not implement live error validation, maybe in future issue? Pop up message will display when email is successfully sent(fades away after 5 sec).

---
+++ Models/EmailSenderModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace srk_website.Models
+{
+    public class EmailSenderModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+        public string? Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string? Phone { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+        public string? Subject { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 10)]
+        public string? Message { get; set; }
+    }
+}
