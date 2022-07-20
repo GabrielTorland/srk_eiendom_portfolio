cf5d3e6c4873ce90150a900f60a82f43485c7054
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2022-07-20 23:34:52 +0200

    Web app is now connected to azure sql server

--- Program.cs
+++ Program.cs
@@ -8,10 +8,34 @@ using srk_website.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Key vault options
+SecretClientOptions options = new SecretClientOptions()
+{
+    Retry =
+            {
+                Delay= TimeSpan.FromSeconds(2),
+                MaxDelay = TimeSpan.FromSeconds(16),
+                MaxRetries = 5,
+                Mode = RetryMode.Exponential
+            }
+};
+// Key vault connection
+var kVUrl = builder.Configuration.GetValue<string>("AzureKeyVaultUrl");
+var secretsClient = new SecretClient(new Uri(kVUrl), new DefaultAzureCredential(), options);
+
+// Get BlobConnectionString from key vault and store it in appsettings.json
+builder.Configuration["BlobConnectionString"] = secretsClient.GetSecret("BlobConnectionString").Value.Value;
+
+// Get SendGridKey from key vault and store it in appsettings.json
+builder.Configuration["SendGridKey"] = secretsClient.GetSecret("SendGridKey").Value.Value;
+
+// Get MySQL connection string from key vault and store it in appsettings.json
+builder.Configuration["ConnectionStrings:DefaultConnection"] = secretsClient.GetSecret("DefaultConnectionString").Value.Value;
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
@@ -21,26 +45,6 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IGenerateRandomImageName, GenerateRandomImageName>();
 builder.Services.AddTransient<IAzureStorage, AzureStorage>();
 builder.Services.AddTransient<IEmailSender, EmailSender>();
-
-// Key vault options
-SecretClientOptions options = new SecretClientOptions()
-{
-    Retry =
-                {
-                    Delay= TimeSpan.FromSeconds(2),
-                    MaxDelay = TimeSpan.FromSeconds(16),
-                    MaxRetries = 5,
-                    Mode = RetryMode.Exponential
-                 }
-};
-// Key vault connection
-var kVUrl = builder.Configuration.GetValue<string>("AzureKeyVaultUrl");
-var secretsClient = new SecretClient(new Uri(kVUrl), new DefaultAzureCredential(), options);
-
-// Get BlobConnectionString from key vault and store it in appsettings.json
-builder.Configuration["BlobConnectionString"] = secretsClient.GetSecret("BlobConnectionString").Value.Value;
-
-builder.Configuration["SendGridKey"] = secretsClient.GetSecret("SendGridKey").Value.Value;
 
 
 // The default inactivity timeout is 14 days. The following code sets the inactivity timeout to 5 days:
@@ -53,7 +57,11 @@ builder.Services.ConfigureApplicationCookie(o => {
 builder.Services.Configure<DataProtectionTokenProviderOptions>(o =>
        o.TokenLifespan = TimeSpan.FromHours(3));
 
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
+
+app.MapHealthChecks("/health");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -94,7 +102,7 @@ using (var scope = app.Services.CreateScope())
     var dataContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     var um = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
     var _emailSender = scope.ServiceProvider.GetRequiredService<IEmailSender>();
-    ApplicationDbInitializer.Initialize(dataContext, um, _emailSender);
+    await ApplicationDbInitializer.Initialize(dataContext, um, _emailSender);
 }
 
 app.Run();
