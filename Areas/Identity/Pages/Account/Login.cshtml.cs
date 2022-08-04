87816d28064172f27346978c0e8647d3828e0774
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2022-08-04 18:17:44 +0200

    login redirect
    The admin will now be redirected to the admin page when signed in

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -115,7 +115,7 @@ namespace srk_website.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return LocalRedirect("~/Admin");
                 }
                 if (result.RequiresTwoFactor)
                 {
